ee0947fd64262c3e0b773d30584b1f5918759975
Azdren Ymeri <azdreny@gmail.com>
2019-08-28 13:41:07 +0200

    Improved error handling and formatted the code

--- src/modules/open-weather-api.js
+++ src/modules/open-weather-api.js
@@ -1,13 +1,11 @@
-
-const searchWeatherByCity = async (cityName) => {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${
-    cityName
-  }&appid=${
-    process.env.WEATHER_API_KEY
-  }&units=metric`;
-
-  const response = await fetch(url);
-  return response.json();
+const searchWeatherByCity = async cityName => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.WEATHER_API_KEY}&units=metric`;
+  try {
+    const response = await fetch(url);
+    return response.json();
+  } catch (err) {
+    return err;
+  }
 };
 
 export default searchWeatherByCity;
